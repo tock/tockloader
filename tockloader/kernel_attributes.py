b6af0ce4e1a8a811bc9e84ddc0580f1745191705
Brad Campbell <bradjc5@gmail.com>
2023-08-05 17:27:46 -0400

    add version and kernel binary kernel attr

--- tockloader/kernel_attributes.py
+++ tockloader/kernel_attributes.py
@@ -1,8 +1,13 @@
+"""
+Parse kernel attributes at the end of the kernel flash region.
+"""
+
 import struct
 
 
 class KATLV:
     TYPE_APP_MEMORY = 0x0101
+    TYPE_KERNEL_BINARY = 0x0102
 
     def get_tlvid(self):
         return self.TLVID
@@ -13,6 +18,7 @@ class KATLV:
 
 class KATLVAppMemory(KATLV):
     TLVID = KATLV.TYPE_APP_MEMORY
+    SIZE = 8
 
     def __init__(self, buffer):
         self.valid = False
@@ -33,7 +39,7 @@ class KATLVAppMemory(KATLV):
         )
 
     def __str__(self):
-        out = "KATLV: App Memory ({})\n".format(self.TLVID)
+        out = "KATLV: App Memory ({:#x})\n".format(self.TLVID)
         out += "  {:<20}: {:>10} {:>#12x}\n".format(
             "app_memory_start", self.app_memory_start, self.app_memory_start
         )
@@ -51,11 +57,54 @@ class KATLVAppMemory(KATLV):
         }
 
 
+class KATLVKernelBinary(KATLV):
+    TLVID = KATLV.TYPE_KERNEL_BINARY
+    SIZE = 8
+
+    def __init__(self, buffer):
+        self.valid = False
+
+        if len(buffer) == 8:
+            base = struct.unpack("<II", buffer)
+            self.kernel_binary_start = base[0]
+            self.kernel_binary_len = base[1]
+            self.valid = True
+
+    def pack(self):
+        return struct.pack(
+            "<IIHH",
+            self.kernel_binary_start,
+            self.kernel_binary_len,
+            self.TLVID,
+            8,
+        )
+
+    def __str__(self):
+        out = "KATLV: Kernel Binary ({:#x})\n".format(self.TLVID)
+        out += "  {:<20}: {:>10} {:>#12x}\n".format(
+            "kernel_binary_start", self.kernel_binary_start, self.kernel_binary_start
+        )
+        out += "  {:<20}: {:>10} {:>#12x}\n".format(
+            "kernel_binary_len", self.kernel_binary_len, self.kernel_binary_len
+        )
+        return out
+
+    def object(self):
+        return {
+            "type": "kernel_binary",
+            "id": self.TLVID,
+            "kernel_binary_start": self.kernel_binary_start,
+            "kernel_binary_len": self.kernel_binary_len,
+        }
+
+
 class KernelAttributes:
     """
     Represent attributes stored at the end of the kernel image that contain metadata
     about the installed kernel.
     """
+
+    KATLV_TYPES = [KATLVAppMemory, KATLVKernelBinary]
 
     def __init__(self, buffer):
         self.tlvs = []
@@ -71,17 +120,26 @@ class KernelAttributes:
             return
         buffer = buffer[:-4]
 
-        while len(buffer) > 4:
-            # Now try to parse TLVs, but going backwards in flash.
-            t, l = struct.unpack("<HH", buffer[-4:])
-            buffer = buffer[:-4]
+        # Parse the version number.
+        version = struct.unpack("<B", buffer[-1:])[0]
+        # Skip the version and three reserved bytes.
+        buffer = buffer[:-4]
 
-            if t == KATLV.TYPE_APP_MEMORY:
-                if len(buffer) >= 8 and l == 8:
-                    self.tlvs.append(KATLVAppMemory(buffer[-8:]))
-                    buffer = buffer[:-8]
-            else:
-                break
+        if version == 1:
+            while len(buffer) > 4:
+                # Now try to parse TLVs, but going backwards in flash.
+                t, l = struct.unpack("<HH", buffer[-4:])
+                buffer = buffer[:-4]
+
+                for katlv_type in self.KATLV_TYPES:
+                    if t == katlv_type.TLVID:
+                        katlv_len = katlv_type.SIZE
+                        if len(buffer) >= katlv_len and l == katlv_len:
+                            self.tlvs.append(katlv_type(buffer[-1 * katlv_len :]))
+                            buffer = buffer[: -1 * katlv_len]
+                        break
+                else:
+                    break
 
     def get_app_memory_region(self):
         """

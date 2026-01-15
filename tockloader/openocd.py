f5ebf66ea42db1a0e0aa53604be21ad23c18c53f
Tyler Potyondy <potyondyt@gmail.com>
2026-01-15 11:57:40 -0800

    boards: add stm32wle5jc to known boards
    This adds the stm32wle5jc chip used in the seeed_studio_lora-E5-HF dev
    board to tockloader's known board list. Communication intended to occur
    using stlink over openocd.

--- tockloader/openocd.py
+++ tockloader/openocd.py
@@ -267,7 +267,7 @@ You may need to update OpenOCD to the version in latest git master."
             ("(mfg: 0x049 (Xilinx), part: 0x3631, ver: 0x1)", "arty"),
             ("SWD DPIDR 0x2ba01477", "microbit_v2"),
             ("stm32f4x.cpu", "stm32f4discovery"),
-            ("stm32wlx.cpu", "stm32wle5jc")
+            ("stm32wlx.cpu", "stm32wle5jc"),
         ]
 
         emulators = []
